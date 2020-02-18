755c2017e985b5b7f8047989aacf36cf7f3a6fd4
SijoKappen <sijo.kappen@gmail.com>
2020-02-19 09:39:43 +1100

    Update takehome.py

--- takehome.py
+++ takehome.py
@@ -1,24 +1,28 @@
-
-
-## Functional Arrays
+# Functional Arrays
 import numpy as np
+
 
 def create_array_from_function(f, d, dtype=None):
     a = np.array([f(i, j) for i in range(d[0]) for j in range(d[1])], dtype)
     return a.reshape(d)
 
-print(create_array_from_function(lambda i,j: (i - j)**2, [4, 4]))
+
+print(create_array_from_function(lambda i, j: (i - j) ** 2, [4, 4]))
 
 
-
-
-
-## Removing Boundaries
+# Removing Boundaries
 def boundary_cropping(a, m):
-	return a[np.ix_(*map(lambda e: range(e.min(), e.max() + 1),np.where(m)))]
+    return a[np.ix_(*map(lambda e: range(e.min(), e.max() + 1), np.where(m)))]
 
-a1 = np.array([[0,0,0,0,0], [0,0,0,0,0], [0,1,0,1,1], [0,0,0,0,0]])
-a2 = np.array([[ [0,0,0], [0,1,0], [0,1,0] ], [ [0,0,0], [0,1,0], [0,0,0] ], [ [0,0,0], [0,1,0], [0,0,0] ]])
+
+a1 = np.array([[0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 1, 0, 1, 1], [0, 0, 0, 0, 0]])
+a2 = np.array(
+    [
+        [[0, 0, 0], [0, 1, 0], [0, 1, 0]],
+        [[0, 0, 0], [0, 1, 0], [0, 0, 0]],
+        [[0, 0, 0], [0, 1, 0], [0, 0, 0]],
+    ]
+)
 
 print(boundary_cropping(a1, a1 != 0))
 # [[1 0 1 1]]
@@ -26,9 +30,7 @@ print(boundary_cropping(a2, a2 != 0))
 # [[[1] [1]] [[1] [0]] [[1] [0]]]
 
 
-
-
-## Block Reshaping
+# Block Reshaping
 def get_a_block(a, r, c):
     pick_count = 0
     skip_count = 0
@@ -51,6 +53,7 @@ def get_a_block(a, r, c):
     block.append(row)
     return block
 
+
 def shape_as_blocks(a, r, c):
     if len(a.flat) % (r * c) != 0:
         print("Invalid block size")
@@ -65,66 +68,67 @@ def shape_as_blocks(a, r, c):
     result_arr = np.array(result)
     return result_arr
 
-arr = np.array([[1,2,3,4], [5,6,7,8], [9,0,1,2]])
+
+arr = np.array([[1, 2, 3, 4], [5, 6, 7, 8], [9, 0, 1, 2]])
 print(shape_as_blocks(arr, 2, 2))
 
 
-
-## Population Variance from Subpopulation Variance
+# Population Variance from Subpopulation Variance
 def pop_var_from_subpop_var(groups):
-    variance  = np.array( [ np.var(group)for group in groups] )
-    averages = np.array( [ np.mean(group)for group in groups] )
-    counts = np.array( [ np.size(group)for group in groups] )
+    variance = np.array([np.var(group) for group in groups])
+    averages = np.array([np.mean(group) for group in groups])
+    counts = np.array([np.size(group) for group in groups])
     average = np.average(averages, weights=counts)
     size = 0
     for count in counts:
         size += count
-    return np.sum((counts) * variance + counts * (averages - average)**2) / (size)
-   
-groups = [np.array([1,2,3,4]), np.array([5,6])]
+    return np.sum((counts) * variance + counts * (averages - average) ** 2) / (size)
+
+
+groups = [np.array([1, 2, 3, 4]), np.array([5, 6])]
 print(pop_var_from_subpop_var(groups))
 # 2.9166666666666665
 
 
-
-## Shuffle a Large List
+# Shuffle a Large List
 import random
 
-l = [1,2,3,4,5]
+l = [1, 2, 3, 4, 5]
+
 
 def shuffle_list_inplace_constant_memory(l):
-    for i in range((len(l)-1),0,-1):
-        j = random.randint(0,i)
-        l[i],l[j] = l[j],l[i]
+    for i in range((len(l) - 1), 0, -1):
+        j = random.randint(0, i)
+        l[i], l[j] = l[j], l[i]
+
+
 shuffle_list_inplace_constant_memory(l)
 print(l)
 
 
-
-
-
-## Acquiring Coordinates
+# Acquiring Coordinates
 def coordinates_from_steps(a, s, dtype=int):
     result = list()
-    rowCount,ColumnCount = (0,0)
+    rowCount, ColumnCount = (0, 0)
     for index, value in np.ndenumerate(a):
-        if ( rowCount == 0 ) and ( ColumnCount == 0 ):
+        if (rowCount == 0) and (ColumnCount == 0):
             result.append(list(index))
-        if(index[1] == 0):
+        if index[1] == 0:
             rowCount += 1
-        if( rowCount >= s[0]):
+        if rowCount >= s[0]:
             rowCount = 0
         ColumnCount += 1
-        if( ColumnCount >= s[1]):
+        if ColumnCount >= s[1]:
             ColumnCount = 0
     return np.array(result)
 
-print(coordinates_from_steps(np.array([[1,2],[3,4]]), (1,1)))
+
+print(coordinates_from_steps(np.array([[1, 2], [3, 4]]), (1, 1)))
 # [[0 0]
 #  [0 1]
 #  [1 0]
 #  [1 1]]
 
-print(coordinates_from_steps(np.array([[1,2],[3,4]]), (1,2)))
+print(coordinates_from_steps(np.array([[1, 2], [3, 4]]), (1, 2)))
 # [[0 0]
 #  [1 0]]
